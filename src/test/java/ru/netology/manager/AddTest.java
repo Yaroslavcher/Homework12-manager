8468d82b17b44a5b3d03061cd18d78ddaf28fe07
Ярослав Черников <yaroslavcher@gmail.com>
2022-07-21 22:30:10 +0300

    mockito not exist

--- src/test/java/ru/netology/manager/AddTest.java
+++ src/test/java/ru/netology/manager/AddTest.java
@@ -7,20 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AddTest {
     Repository repo = new Repository();
     Manager manager = new Manager(repo);
+    Films film1 = new Films(1, "name1");
+    Films film2 = new Films(2, "name2");
+    Films film3 = new Films(3, "name3");
 
-/*    @Test
+    @Test
     public void shouldAddFilm() {
-        manager.add("film1");
-
-        String[] expected = {"film1"};
-        assertArrayEquals(repo.findAll(), expected);
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        Films film4 = new Films(4, "name4");
+        manager.add(film4);
+        Films[] films = {film1, film2, film3, film4};
+        Films[] expected = {film1, film2, film3, film4};
+        assertArrayEquals(expected, repo.findAll());
     }
 
     @Test
     public void shouldAddNone() {
         String[] expected = {};
-        assertArrayEquals(manager.findAll(), expected);
-    }*/
+        assertArrayEquals(repo.findAll(), expected);
+    }
 
 
 }
--- src/test/java/ru/netology/manager/ManagerTest.java
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -1,33 +1,19 @@
 package ru.netology.manager;
 
-import org.mockito.Mockito;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class ManagerTest {
-/*    Repository repo = Mockito.mock(Repository.class);*/
-    Repository repo = new Repository();
+    Repository repo = Mockito.mock(Repository.class);
     Manager manager = new Manager(repo);
     Films film1 = new Films(1, "name1");
     Films film2 = new Films(2, "name2");
     Films film3 = new Films(3, "name3");
 
     Films[] films = {film1, film2, film3};
-
-    @Test
-    public void shouldAddFilm() {
-        manager.add(film1);
-        manager.add(film2);
-        manager.add(film3);
-        Films film4 = new Films(4, "name4");
-        manager.add(film4);
-        Films[] films = {film1, film2, film3, film4};
-/*        doReturn(films).when(repo).findAll();*/
-        Films[] expected = {film1, film2, film3, film4};
-        assertArrayEquals(expected, repo.findAll());
-    }
 
     @Test
     public void shouldFindLastTwoFilms() {
@@ -50,6 +36,4 @@ public class ManagerTest {
         Films[] expected = {film3};
         assertArrayEquals(expected, manager.findLast());
     }
-
-
 }
